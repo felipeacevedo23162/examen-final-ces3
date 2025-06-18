2b05079a16b31ec6c7bfa114edd1cd0762979d6f
felipeacevedo23162 <133995596+felipeacevedo23162@users.noreply.github.com>
2025-06-18 18:58:02 -0500

    Initial project setup and dependencies

---
+++ src/main/java/co/edu/elpoli/ces3/examenfinalces3/ExamenFinalCes3Application.java
@@ -0,0 +1,13 @@
+package co.edu.elpoli.ces3.examenfinalces3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenFinalCes3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenFinalCes3Application.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/elpoli/ces3/examenfinalces3/ExamenFinalCes3ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.elpoli.ces3.examenfinalces3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenFinalCes3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
