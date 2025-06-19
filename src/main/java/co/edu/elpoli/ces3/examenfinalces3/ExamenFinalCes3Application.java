7974dd70593846d800468d038805a1d15eac75a7
felipeacevedo23162 <133995596+felipeacevedo23162@users.noreply.github.com>
2025-06-18 20:41:08 -0500

    Implement TeacherRepository and basic CRUD

--- src/main/java/co/edu/elpoli/ces3/examenfinalces3/ExamenFinalCes3Application.java
+++ src/main/java/co/edu/elpoli/ces3/examenfinalces3/ExamenFinalCes3Application.java
@@ -2,6 +2,10 @@ package co.edu.elpoli.ces3.examenfinalces3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import co.edu.elpoli.ces3.examenfinalces3.model.Teacher;
+import co.edu.elpoli.ces3.examenfinalces3.repository.TeacherRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ExamenFinalCes3Application {
@@ -10,4 +14,16 @@ public class ExamenFinalCes3Application {
         SpringApplication.run(ExamenFinalCes3Application.class, args);
     }
 
+    @Bean
+    public CommandLineRunner demo(TeacherRepository repo) {
+        return args -> {
+            repo.save(new Teacher("Felipe", "Acevedo", "Matemáticas", 3000000.0, "felipe@ejemplo.com"));
+            repo.save(new Teacher("Laura", "García", "Inglés", 2800000.0, "laura@ejemplo.com"));
+
+            repo.findAll().forEach(t -> {
+                System.out.println(t);
+                System.out.println("Salario anual: " + t.calcularSalarioAnual());
+            });
+        };
+    }
 }
--- src/main/java/co/edu/elpoli/ces3/examenfinalces3/repository/TeacherRepository.java
+++ src/main/java/co/edu/elpoli/ces3/examenfinalces3/repository/TeacherRepository.java
@@ -2,7 +2,15 @@ package co.edu.elpoli.ces3.examenfinalces3.repository;
 
 import co.edu.elpoli.ces3.examenfinalces3.model.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
-    // Puedes agregar @Query personalizados aquí si deseas
+
+
+    @Query("SELECT t FROM Teacher t WHERE t.salary > ?1")
+    List<Teacher> findTeachersWithSalaryGreaterThan(Double salary);
+
+    @Query("SELECT t FROM Teacher t WHERE t.subject = ?1")
+    List<Teacher> findBySubject(String subject);
 }
